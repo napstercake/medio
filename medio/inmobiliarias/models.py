fe243eead1bd9a770bbc8dc563c7ce1c1c499bbb
napstercake <ricardo.gzb@outlook.com>
2014-07-17 16:31:58 -0500

    Adding new filed called nombre_comercial and setting some attributes to optional

--- medio/inmobiliarias/models.py
+++ medio/inmobiliarias/models.py
@@ -9,14 +9,15 @@ from django.db import models
 
 class Inmobiliaria(models.Model):
 	razon_social = models.CharField(max_length=255)
+	nombre_comercial = models.CharField(max_length=255, blank=True)
 	ruc = models.BigIntegerField()
 	direccion = models.CharField(max_length=255)
 	telefono_fijo_1 = models.CharField(max_length=30)
 	telefono_fijo_2 = models.CharField(max_length=30, blank=True)
-	telefono_celular_1 = models.CharField(max_length=50)
+	telefono_celular_1 = models.CharField(max_length=50, blank=True)
 	telefono_celular_2 = models.CharField(max_length=50, blank=True)
 	telefono_celular_3 = models.CharField(max_length=50, blank=True)
-	web = models.CharField(max_length=255)
+	web = models.CharField(max_length=255, blank=True)
 	logo = models.ImageField(upload_to='img')
 
 	def __unicode__(self):
